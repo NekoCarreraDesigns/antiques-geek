aabf7577daf95849217bcc504f5ceae1db5feb30
NekoCarreraDesigns <denvermaas@gmail.com>
2023-11-07 16:21:46 -0700

    changed anchor tags to Link

--- antiques-geek/src/components/Stamps-View/index.js
+++ antiques-geek/src/components/Stamps-View/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function StampsView() {
   return (
@@ -11,9 +12,9 @@ function StampsView() {
             1870 3 cent George Washington <br /> UNHVF $146.75
           </p>
           <br />
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1894_Postage_Due.jpg' />
@@ -21,9 +22,9 @@ function StampsView() {
             1894 1 cent postage due <br /> MNHXF $700
           </p>
           <br />
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1921_George_Washington.jpg' />
@@ -32,19 +33,19 @@ function StampsView() {
             1921 George Washing block of 2
             <br /> UNHXF $70,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1923_George_Washington.jpg' />
           <p className='description'>
-            1923 2 cent George Washington two sides imperforated
+            1923 2 cent George Washington two sides un-imperforated
             <br /> UNHVF $2,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
       </div>
       <div className='stamp-row-1'>
@@ -54,9 +55,9 @@ function StampsView() {
             1923 4 cent Canal Zone Biplane
             <br /> UNHVF $37.50
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1932_George_Washington.jpg' />
@@ -64,9 +65,9 @@ function StampsView() {
             1932 3 cent George Washington imperforated on two sides <br />{" "}
             UNHVf/XF $3,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1945_Iwo_Jima.jpg' />
@@ -74,9 +75,9 @@ function StampsView() {
             1945 3 cent Iwo Jima block of 4
             <br /> MNHXFOG $600
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1884_Russian_Coat_of_Arms_3.5.jpg' />
@@ -84,9 +85,9 @@ function StampsView() {
             1884 Russian coat of arms 3.5 kopeks
             <br /> MNHSUOG $90,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
       </div>
       <div className='stamp-row-2'>
@@ -96,9 +97,9 @@ function StampsView() {
             1884 Russian coat of arms 7 kopeks
             <br /> MNHSUOG $36,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1889_Coat_of_Arms_20.jpg' />
@@ -106,9 +107,9 @@ function StampsView() {
             1889 Russian coat of arms 20 kopeks
             <br /> MNHXFOG $450
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1889_Coat_of_Arms.jpg' />
@@ -116,9 +117,9 @@ function StampsView() {
             1889 Russian coat of arms 50 kopeks
             <br /> MNHXFOG $450
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
         <div className='stamp-column'>
           <img alt='stamp' src='./assets/1908_Coat_of_Arms.jpg' />
@@ -126,9 +127,9 @@ function StampsView() {
             1908 Russian coat of arms 10 kopeks
             <br /> MNHSUOG $3,000
           </p>
-          <a className='purchase-link' href='/purchase-message'>
+          <Link className='purchase-link' to='/purchase-message'>
             Purchase
-          </a>
+          </Link>
         </div>
       </div>
     </div>
